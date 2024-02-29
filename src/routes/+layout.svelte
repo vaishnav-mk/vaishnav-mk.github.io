23ec291562dca26af03313078e5241b2b98231d6
vaishnav-mk <vaishnav240204@gmail.com>
2024-02-29 22:43:42 +0530

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,6 @@
 	<meta name="theme-color" content="#000000" />
 	<meta name="msapplication-TileColor" content="#000000" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="icon" href="/favicon.ico" />
 </svelte:head>
 
 <slot />
