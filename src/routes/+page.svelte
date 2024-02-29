527c7b07e7be448c7508fd316820ffe2f0de9a1c
vaishnav-mk <vaishnav240204@gmail.com>
2024-02-29 22:34:27 +0530

    more guards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,6 @@
 	import About from '../components/Sections/About.svelte';
 	import Section from '../components/Sections/Section.svelte';
 
-	import ProjectCard from '../components/Cards/ProjectCard.svelte';
-	import ExperienceCard from '../components/Cards/ExperienceCard.svelte';
-	import CommunityCard from '../components/Cards/CommunityCard.svelte';
-
 	import Nav from '../components/Nav.svelte';
 	import Sidebar from '../components/Sidebar.svelte';
 	import MobileSidebar from '../components/MobileSidebar.svelte';
@@ -18,14 +14,20 @@
 </script>
 
 <div>
-	<Nav {NavData} />
+	{#if NavData}
+		<Nav {NavData} />
+	{/if}
 	<div class="flex flex-col md:flex-row max-w-7xl md:mx-auto md:gap-4 md:m-4">
 		<Sidebar data={info} />
-		<MobileSidebar data={info} />
+		{#if NavData}
+			<MobileSidebar data={info} />
+		{/if}
 		<main class="md:w-3/4 overflow-y-auto p-10">
 			<div class="min-w-screen min-h-screen">
 				<div class="container mx-auto max-w-5xl mt-5">
-					<About data={info} />
+					{#if info}
+						<About data={info} />
+					{/if}
 					{#each sections as sectionData}
 						<Section {sectionData} />
 					{/each}
@@ -33,7 +35,7 @@
 			</div>
 		</main>
 	</div>
-	{#if data.footer}
+	{#if FooterData}
 		<Footer data={FooterData} />
 	{/if}
 </div>
