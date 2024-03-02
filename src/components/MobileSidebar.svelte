1ae2f4e908720bab04c23ceb10d68e2fd2357917
vaishnav-mk <vaishnav240204@gmail.com>
2024-03-02 23:04:18 +0530

    side nabar

--- src/components/MobileSidebar.svelte
+++ src/components/MobileSidebar.svelte
@@ -5,7 +5,7 @@
 	const img_url = images[`/src/lib/images/${data?.image || 'srm2.png'}`]?.default;
 </script>
 
-<aside class="w-full flex md:hidden gap-2 p-2 bg-zinc-100 border-b-1">
+<aside class="w-full flex md:hidden gap-2 p-2 bg-zinc-100 border-b-1 dark:border-indigo-950 dark:text-slate-400 dark:bg-slate-800">
 	<div class="">
 		<img src={img_url} alt="Vaishnav Manoj" class="w-14 h-14 rounded-full" />
 	</div>
