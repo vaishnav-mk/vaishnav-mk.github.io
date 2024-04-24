683e719bd65dea0b10586ef61f0c075405a0f1e1
vaishnav-mk <vaishnav240204@gmail.com>
2024-04-24 17:24:03 +0530

    fix: glob imports error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+import * as path from "path";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	extension: ['.yml', '.yaml'],
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	alias: {
+		'@assets': path.resolve(__dirname, 'src/lib/images')
+	}
 });
