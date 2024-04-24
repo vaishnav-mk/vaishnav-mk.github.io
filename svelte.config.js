8f38c4f6097ae16636f10655f2519358334d7ad8
vaishnav-mk <vaishnav240204@gmail.com>
2024-04-24 17:11:42 +0530

    fix: files breaking randomly

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
-
 export default config;
